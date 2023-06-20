614644df9477a8b65db93723a9ec6ce8665e3a19
MADHU SRIRAM SV <75003175+madhusriram012@users.noreply.github.com>
2023-06-20 10:42:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/",function(req,res){
 
 app.post("/",function(req,res){
     const query=req.body.cityName;
-    const apiKey="243282079b11d9eb88cff3eb128fed98"
+    const apiKey="Type your own api key instead of this";
     const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units=metric";
     https.get(url,function(response){
         console.log(response.statusCode)
